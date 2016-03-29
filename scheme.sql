47436bd0a5e6dd97f5dd7f834cc97442b9c688a6
He Buwei <hebuweitom@gmail.com>
2016-03-29 18:28:51 +0800

    fix schema

--- scheme.sql
+++ scheme.sql
@@ -1,45 +1,49 @@
+DROP TABLE IF EXISTS funding;
+DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS funder;
+DROP TABLE IF EXISTS entrepreneur;
+DROP TABLE IF EXISTS admin;
+
+
 CREATE TABLE funder (
 name VARCHAR(255) PRIMARY KEY,
-password VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS entrepreneur;
+
 CREATE TABLE entrepreneur (
 name VARCHAR(255) PRIMARY KEY,
-password VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS admin;
+
 CREATE TABLE admin (
 name VARCHAR(255) PRIMARY KEY,
-password VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
 );
 
 
-DROP TABLE IF EXISTS project;
 CREATE TABLE project (
+id INT PRIMARY KEY,
 title VARCHAR(255) NOT NULL,
 description TEXT,
 owner VARCHAR(255) NOT NULL,
 start_date DATE NOT NULL,
 end_date DATE NOT NULL,
-category VARCHAR(255) NOT NULL,
+category VARCHAR(255) NOT NULL, -- TODO use enum?
 target_amount INT NOT NULL,
 FOREIGN KEY (owner) REFERENCES entrepreneur(name) ON UPDATE CASCADE,
 CHECK (target_amount > 0),
-CHECK (end_date > start_date),
-CHECK (category = ""),
+CHECK (end_date > start_date)
 );
 
-DROP TABLE IF EXISTS funding;
+
 CREATE TABLE funding (
 funding_id INT PRIMARY KEY,
 funder_name VARCHAR(255) NOT NULL,
-project_title VARCHAR(255) NOT NULL,
+project_id INT NOT NULL,
 amount INT NOT NULL,
 FOREIGN KEY (funder_name) REFERENCES funder(name) ON UPDATE CASCADE,
-FOREIGN KEY (project_title) REFERENCES project(title) ON UPDATE CASCADE,
-CHECK (amount > 0),
+FOREIGN KEY (project_id) REFERENCES project(id) ON UPDATE CASCADE,
+CHECK (amount > 0)
 );
-
