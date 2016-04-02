e08dde9c1b79864eff23a4e441e6a59f64915c61
Yanjuan <duanyanjuan@DuAns-MacBook-Pro.local>
2016-04-02 18:54:00 +0800

    Revert "data"
    This reverts commit ed6a644643296008a28599b63e5f5588ee8a4b06.

--- data/project_data.sql
+++ data/project_data.sql
@@ -1,4 +1,6 @@
-INSERT INTO project VALUES ('1','primaryschool','estabilish a primary school','growup','2015-11-23','2016-11-23','education','34500');
-INSERT INTO project VALUES ('2','plant','establish a public park','green','2016-02-23','2016-07-23','56700');
-INSERT INTO project VALUES ('3','sing','hold a singing competition','singout','2016-03-03','2016-05-20','art','5600');
-INSERT INTO project VALUES ('4','computers','need sufficient hardwares to start up','srartup','2016-01-04','2017-04-20','technologu','578900');
+INSERT INTO project VALUES ('primaryschool','estabilish a primary school','growup','2015-11-23','2016-11-23','education','345000','0',true);
+INSERT INTO project VALUES ('secondaryschool','estabilish a secondary school','growup','2015-09-12','2016-09-23','education','900000','0',true);
+INSERT INTO project VALUES ('kindergarten','estabilish a kindergarten','growup','2014-01-12','2015-09-23','education','6800000','0',false);
+INSERT INTO project VALUES ('plant','establish a public park','green','2016-02-23','2016-07-23','environment','567000','0',true);
+INSERT INTO project VALUES ('sing','hold a singing competition','singout','2016-03-03','2016-05-20','art','560000','0',true);
+INSERT INTO project VALUES ('computers','need sufficient hardwares to start up','startup','2016-01-04','2017-04-20','technology','5789000','0',true);
