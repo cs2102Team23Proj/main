8daa96ac394517ad3910d37457de62b402bd2904
Yanjuan <duanyanjuan@DuAns-MacBook-Pro.local>
2016-04-02 15:23:55 +0800

    02/04 15:23

--- queries.sql
+++ queries.sql
@@ -1,10 +1,21 @@
---1 List out all the projects currently looing for crodfunding 
+--List out all the valid users
+SELECT * FROM user;
+
+--List out all the projects currently looing for crodfunding 
 SELECT * FROM project WHERE DATE(end_data) > CURDATE();
 
---2 List out all the projects in the education category
+--List out all the projects in the education category
 SELECT * FROM project p WHERE p.category = 'education';
 
---3 List out all the projects' details of user buwei
+--List out all the projects user buwei has donated money to
 SELECT p.id, p.title, p.description, p.owner, p.start_date, p.end_date, p.category, f.amount 
-FROM project p, funding f, user u
-WHERE f.project_id = p.id AND f.user_name = u.name; 
+FROM project p, funding f
+WHERE f.project_id = p.id AND f.user_name = 'buwei';
+
+--List out all the users who have donated money to project plant
+SELECT u.name, f.amount 
+FROM user u, funding f, project p
+WHERE f.user_name = u.name AND f.project_id = p.id AND p.title = 'plant';
+
+--List out all the projects related to entrepreneur green
+SELECT * FROM project p WHERE p.owner = 'green';
