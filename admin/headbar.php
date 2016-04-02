17d2a75d40983dbdb2e8453dfcd9291df8f94f4b
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2016-04-02 17:28:44 +0800

    change name of the project
    叫 Fund Start Here
    by yanjuan~

--- admin/headbar.php
+++ admin/headbar.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">起名字啊啊</a>
+          <a class="navbar-brand" href="index.html">Fund Start Here</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
