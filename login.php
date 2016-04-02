c1eb4b7bc3b1e37ce963d37b723c0194da530247
He Buwei <hebuweitom@gmail.com>
2016-04-03 02:38:37 +0800

    login / logout

---
+++ login.php
@@ -0,0 +1,92 @@
+<?php 
+session_start();
+
+include_once 'db_connect.php';
+
+if (isset($_POST['btn-login'])) {
+  $email = pg_escape_string($_POST['email']);
+  $password = pg_escape_string($_POST['password']);
+  $domain = pg_escape_string($_POST['domain']);
+
+  if ($domain==="user") {
+    $result = pg_query("SELECT email, name, password FROM funder WHERE email='$email'");
+    if (pg_num_rows($result)==1) {
+      $row = pg_fetch_row($result);
+      if (password_verify($password, $row[2])) { // pass
+        $_SESSION['domain'] = $domain;
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $row[1];
+        header("Location: index.php");
+      } else {
+        auth_fail();
+      }
+    } else {
+      auth_fail();
+    }
+  } elseif ($domain==="entrepreneur") {
+    $result = pg_query("SELECT email, name, password FROM entrepreneur WHERE email='$email'");
+    if (pg_num_rows($result)==1) {
+      $row = pg_fetch_row($result);
+      if (password_verify($password, $row[2])) { // pass
+        $_SESSION['domain'] = $domain;
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $row[1];
+        header("Location: index.php");
+      } else {
+        auth_fail();
+      }
+    } else {
+      auth_fail();
+    }
+  } else {
+    echo "<script>alert('Domain error');</script>";
+  }
+}
+
+function auth_fail() {
+  echo "<script>alert('Wrong user name or password.');</script>";
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Login</title>
+  <?php require 'head.php';?>
+</head>
+
+
+<body>
+  <?php require 'navbar.php';?>
+
+  <div class="wrap">
+    <h2>Log In</h2>
+    <h4>New Here? <a href="sign_up.php">Create an account</a></h4>
+    <form method="post">
+      <div class="form-group">
+        <label for="input-email">Email</label>
+        <input type="email" class="form-control" id="input-email" name="email" placeholder="Email" required>
+      </div>
+      <div class="form-group">
+        <label for="input-password">Password</label>
+        <input type="password" class="form-control" id="input-password" name="password" placeholder="Password" required>
+      </div>
+      <div class="form-group">
+        <label for="">Login As:</label>
+        <div class="radio">
+          <label><input type="radio" name="domain" value="user" checked>User</label>
+        </div>
+        <div class="radio">
+          <label><input type="radio" name="domain" value="entrepreneur">Entrepreneur</label>
+        </div>
+      <button name="btn-login" type="submit" class="btn btn-default">Log In</button>
+    </form>
+  </div>
+
+  <?php include 'footer.php';?>
+
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,3 +1,8 @@
+<?php 
+session_start();
+
+?>
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -8,13 +13,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.html">起名字啊啊</a>
+      <a class="navbar-brand" href="index.php">起名字啊啊</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="index.html">HOME</a></li>
+        <li><a href="index.php">HOME</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PROJECT<span class="caret"></span></a>
           <ul class="dropdown-menu">
@@ -32,9 +37,15 @@
         <button type="submit" class="btn btn-default">Find</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <!-- 这里应该也要要用逻辑写吧 就是login的名字-->
-        <li><a href="sign_up.php">Sign up</a></li>
-        <li><a href="login.html">Sign in</a></li>
+      <?php 
+      if (isset($_SESSION['domain'])) {
+        echo "<li><a href='my_profile.php'>" . $_SESSION['name'] . ", " . $_SESSION['domain'] . "</a></li>";
+        echo "<li><a href='logout.php'>Log Out</a></li>";
+      } else {
+        echo "<li><a href='login.php'>Log In</a></li>";
+        echo "<li><a href='sign_up.php'>Sign Up</a></li>";
+      }
+      ?>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
