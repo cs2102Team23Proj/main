9fc49ee1999f975822fd74ff554a182322feb437
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2016-04-02 17:26:48 +0800

    admin-manage funder

---
+++ admin/admin_delete_funder.php
@@ -0,0 +1,6 @@
+<?php 
+include_once '../db_connect.php';
+$query = "DELETE FROM funder WHERE name = '" . $_GET['name']. "'";
+pg_query($query);
+header("Location: manage_funder.php");
+?>
---
+++ admin/admin_sidebar.php
@@ -0,0 +1,8 @@
+    <div>
+        <div class="guide-container">
+            <h3>Manage</h3>
+            <ul><a href="manageUser.html">Users</a></ul>
+            <ul><a href="manageEntre.html">Entrepreneurs</a></ul>
+            <ul><a href="manageProj.html">Projects</a></ul>
+        </div>
+    </div>
