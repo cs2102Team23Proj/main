aea21dfac336f8b91a591259819167427189a1e3
Yanjuan <duanyanjuan@DuAns-MacBook-Pro.local>
2016-04-02 18:33:01 +0800

    queries

--- queries.sql
+++ queries.sql
@@ -1,21 +1,35 @@
 --List out all the valid funders
-SELECT * FROM funder;
+SELECT f.name, f.email FROM funder f;
 
 --List out all the projects currently looing for crodfunding 
-SELECT * FROM project WHERE DATE(end_data) > CURDATE();
+SELECT p.title, p.description, p.start_date, p.end_date, p.category, p.target_amount, p.current_amount 
+FROM project p WHERE p.status = true;
 
 --List out all the projects in the education category
-SELECT * FROM project p WHERE p.category = 'education';
+SELECT p.title, p.description, p.start_date, p.end_date, p.category, p.target_amount, p.current_amount, 
+CASE WHEN p.status <> true THEN 'closed' ELSE 'open' END
+AS education
+FROM project p WHERE p.category = 'education';
 
 --List out all the projects funder buwei has donated money to
-SELECT p.title, p.description, p.owner, p.start_date, p.end_date, p.category, f.amount 
+SELECT p.title, p.description, p.owner, p.start_date, p.end_date, p.category, f.amount,
+CASE WHEN p.status <> true THEN 'closed' ELSE 'open' END 
 FROM project p, fund f
 WHERE f.project_title = p.title AND f.funder_name = 'buwei';
 
---List out all the funders who have donated money to project plant
+--List out all the funders who have donated money to project computers
 SELECT u.name, f.amount 
 FROM funder u, fund f, project p
-WHERE f.funder_name = u.name AND f.project_title = p.title AND p.title = 'plant';
+WHERE f.funder_name = u.name AND f.project_title = p.title AND p.title = 'computers';
 
---List out all the projects related to entrepreneur green
-SELECT * FROM project p WHERE p.owner = 'green';
+--List out all the projects related to entrepreneur growup
+SELECT p.title, p.description, p.owner, p.start_date, p.end_date, p.category, p.target_amount, p.current_amount,
+CASE WHEN p.status <> true THEN 'closed' ELSE 'open' END 
+FROM project p WHERE p.owner = 'growup';
+
+--List out all the projects funder yuxin has donated according to increasing order of donating amount
+SELECT p.title, p.description, p.owner, p.start_date, p.end_date, p.category, p.target_amount, p.current_amount,
+CASE WHEN p.status <> true THEN 'closed' ELSE 'open' END 
+FROM project p, fund f
+WHERE f.funder_name = 'yuxin' AND f.project_title = p.title
+ORDER BY f.amount ASC;
