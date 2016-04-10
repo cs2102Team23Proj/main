2dc307f3af90bb43534422557697f39bf7293ea8
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2016-04-10 23:27:10 +0800

    edit project

--- admin/edit_project.php
+++ admin/edit_project.php
@@ -1,5 +1,31 @@
 <?php 
+session_start();
+
 include_once '../db_connect.php';
+
+if (isset($_GET['title'])) {
+  $query = "SELECT title,owner,category,start_date,end_date,target_amount,current_amount, status, description FROM project WHERE title = '" . $_GET['title'] . "';";
+  $result = pg_query($query);
+  $row = pg_fetch_row($result);
+
+} 
+
+if (isset($_POST['btn-confirm'])) {
+  $title = pg_escape_string($_POST['title']);
+  $owner = pg_escape_string($_GET['owner']);
+  $email = pg_escape_string($_GET['category']);
+  $email = pg_escape_string($_GET['start_date']);
+  $email = pg_escape_string($_GET['end_date']);
+  $email = pg_escape_string($_GET['target_amount']);
+  $email = pg_escape_string($_GET['current_amount']);
+  $email = pg_escape_string($_GET['status']);
+  $email = pg_escape_string($_GET['description']);
+
+    $query = "UPDATE project SET owner='$owner', category='$category', start_date='$start_date', end_date='$end_date', target_amount ='$target_amount' , current_amount='$current_amount', statuts='$status', description ='$description' WHERE title = '$title'";
+    pg_query($query);
+    header("Location: project.php");
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -12,48 +38,34 @@ include_once '../db_connect.php';
 
 <body>
   <?php require 'sidebar.php';?>
-
   <div class="admin-manage">
     <h3>Edit Project Information</h3>
+    <h4>Project: <?php echo $_GET["title"];?> </h4>
     <div class="wrap">
-      <form method="post" action="" onsubmit="return validate()">
+      <form method="post">
         <div class="form-group">
-          <label for="input-name">Title</label>
-          <input id="input-name" name="title" type="text" class="form-control" placeholder= <?php echo $_GET["title"];?> >
+          <label for="input-title">Project Title (cannot be changed)</label>
+          <input id="input-title" name="title" class="form-control" value = "<?php echo $_GET["title"];?>" placeholder= <?php echo $_GET["title"];?> >
+
+          <label for="input-owner">Owner</label>
+          <input id="input-owner" name="owner" type="text" class="form-control" value=<?php echo '"' . $row[0] . '"';?> >
+          <label for="input-category">Category</label>
+          <input id="input-category" name="category" type="text" class="form-control" value=<?php echo '"' . $row[1] . '"';?> >
+          <label for="input-start_date">Start Date</label>
+          <input id="input-start_date" name="start_date" type="text" class="form-control" value=<?php echo '"' . $row[2] . '"';?> >
+          <label for="input-end_date">End Date</label>
+          <input id="input-end_date" name="end_date" type="text" class="form-control" value=<?php echo '"' . $row[3] . '"';?> >
+          <label for="input-target_amount">Target Amount</label>
+          <input id="input-target_amount" name="target_amount" type="text" class="form-control" value=<?php echo '"' . $row[4] . '"';?> >
+          <label for="input-current_amount">Current Amount</label>
+          <input id="input-current_amount" name="current_amount" type="text" class="form-control" value=<?php echo '"' . $row[5] . '"';?> >
+          <label for="input-status">Status</label>
+          <input id="input-status" name="status" type="text" class="form-control" value=<?php echo '"' . $row[6] . '"';?> >
+          <label for="input-description">Description</label>
+          <input id="input-description" name="description" type="text" class="form-control" value=<?php echo '"' . $row[7] . '"';?> >
+
         </div>
-        <div class="form-group">
-          <label for="input-email">Owner</label>
-          <input id="input-email" name="owner" type="text" class="form-control" placeholder=<?php echo $_GET["owner"];?>>
-        </div>
-        <div class="form-group">
-          <label for="input-name">Category</label>
-          <input id="input-name" name="category" type="text" class="form-control" placeholder= <?php echo $_GET["category"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">Start Date</label>
-          <input id="input-name" name="start_date" type="text" class="form-control" placeholder= <?php echo $_GET["start_date"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">End Date</label>
-          <input id="input-name" name="end_date" type="text" class="form-control" placeholder= <?php echo $_GET["end_date"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">Target Amount</label>
-          <input id="input-name" name="tartget_amount" type="text" class="form-control" placeholder= <?php echo $_GET["tartget_amount"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">Current Amount</label>
-          <input id="input-name" name="current_amount" type="text" class="form-control" placeholder= <?php echo $_GET["current_amount"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">Status</label>
-          <input id="input-name" name="status" type="text" class="form-control" placeholder= <?php echo $_GET["status"];?> >
-        </div>
-        <div class="form-group">
-          <label for="input-name">Description</label>
-          <input id="input-name" name="descpription" type="text" class="form-control" placeholder= <?php echo $_GET["description"];?> >
-        </div>
-        <button name="btn-signup" type="submit" class="btn btn-default">Confirm</button>
+        <button name="btn-confirm" type="submit" class="btn btn-primary">Confirm</button>
       </form>
     </div>
   </div>
