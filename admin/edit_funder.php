12e21fc9ac3fe3612922b0e019819ac78f0deb4a
He Buwei <hebuweitom@gmail.com>
2016-04-10 21:10:13 +0800

    enable editing of funders

--- admin/edit_funder.php
+++ admin/edit_funder.php
@@ -1,28 +1,30 @@
 <?php 
 session_start();
 
-include_once 'db_connect.php';
+include_once '../db_connect.php';
 
-if (isset($_POST['btn-signup'])) {
+if (isset($_GET['email'])) {
+  $query = "SELECT name, email FROM funder WHERE email = '" . $_GET['email'] . "';";
+  $result = pg_query($query);
+  $row = pg_fetch_row($result);
+
+} 
+
+if (isset($_POST['btn-confirm'])) {
   $name = pg_escape_string($_POST['name']);
-  $email = pg_escape_string($_POST['email']);
+  $email = pg_escape_string($_GET['email']);
 
-  //check for duplicate name 
-
-  
+  //check for duplicate name   
   $duplicate_names = pg_query("SELECT * FROM funder WHERE name='$name' AND email != '$email' ");
   if (pg_num_rows($duplicate_names)>=1) {
-      ?>
-
-      <script>alert('The user name is taken. Please try another name.');</script>
-      <?php 
+    ?>
+    <script>alert('The user name is taken. Please try another name.');</script>
+    <?php 
   }else {
     $query = "UPDATE funder SET name='$name' WHERE email = '$email'";
     pg_query($query);
     header("Location: funder.php");
   }
-    
-  
 }
 ?>
 
@@ -38,14 +40,14 @@ if (isset($_POST['btn-signup'])) {
   <?php require 'sidebar.php';?>
   <div class="admin-manage">
     <h3>Edit Funder Information</h3>
-    <h4>Founder: <?php echo $_GET["email"];?> </h4>
+    <h4>Funder: <?php echo $_GET["email"];?> </h4>
     <div class="wrap">
       <form method="post">
         <div class="form-group">
           <label for="input-name">Name</label>
-          <input id="input-name" name="name" type="text" class="form-control" placeholder= <?php echo $_GET["name"];?> >
+          <input id="input-name" name="name" type="text" class="form-control" value=<?php echo '"' . $row[0] . '"';?> >
         </div>
-        <button name="btn-signup" type="submit" class="btn btn-default">Confirm</button>
+        <button name="btn-confirm" type="submit" class="btn btn-primary">Confirm</button>
       </form>
     </div>
   </div>
--- admin/funder.php
+++ admin/funder.php
@@ -33,7 +33,7 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
             echo "<td>" . $row[0] . "</td>";
             echo "<td>" . $row[1] . "</td>";
             echo '<td>';
-            echo '<a href="edit_funder.php?name=' .$row[0]. '&email=' . $row[1] . '"><button type="button" class="btn btn-primary">Edit</button></a>';
+            echo '<a href="edit_funder.php?email=' . $row[1] . '"><button type="button" class="btn btn-primary">Edit</button></a>';
             echo '<a href="delete_funder.php?email=' . $row[1] . '"><button type="button" class="btn btn-danger">Delete</button></a>';
             echo '</td>';
             echo "</tr>";
